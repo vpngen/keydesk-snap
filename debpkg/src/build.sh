33a1777af1e030bdeafb604387dbc15c408e1a1e
Ingmund Olsson <ingmund@proton.me>
2024-01-16 09:47:36 +0200

    add: add debpkg

---
+++ debpkg/src/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+export CGO_ENABLED=0
+
+go build -C keydesk-snap/cmd/snapshot -o ../../../bin/snapshot
+
+go install github.com/goreleaser/nfpm/v2/cmd/nfpm@latest
+
+nfpm package --config "keydesk/debpkg/nfpm.yaml" --target "${SHARED_BASE}/pkg" --packager deb
+
+chown "${USER_UID}:${USER_UID}" "${SHARED_BASE}/pkg/"*.deb
+
